efc849007aadef446f6c1717730eb3ac00cafbac
Naveen Kumar <naveenkumar16613@gmail.com>
2025-04-20 12:15:07 +0530

    first comit

---
+++ com/corejava/day/A.java
@@ -0,0 +1,17 @@
+package com.corejava.day;
+
+public class A {
+	
+	
+	private int x = 1;
+	private void test() {
+		System.out.println(2);
+	}
+	public static void main(String[] args) {
+		A a1 =  new A();
+		a1.test();
+		System.out.println(a1.x);
+	}
+	
+	
+}
---
+++ com/corejava/day/B.java
@@ -0,0 +1,14 @@
+package com.corejava.day;
+
+import com.corejava.day.session17.A;
+
+public class B  {
+	public static void main(String[] args) {
+		
+		A b1 = new A();
+		System.out.println(b1.x);
+		b1.test();
+		
+	}
+
+}
