8c870ec416e5a3a774d733d398e9cf05889bd6b3
Naveen Kumar <naveenkumar16613@gmail.com>
2025-04-20 12:27:44 +0530

    new file chnage committ

--- com/corejava/day/NewClass.java
+++ com/corejava/day/NewClass.java
@@ -3,6 +3,8 @@ package com.corejava.day;
 public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("new Branch");
+		
+		System.out.println("naveen has chabnges some file");
 	}
 
 }
